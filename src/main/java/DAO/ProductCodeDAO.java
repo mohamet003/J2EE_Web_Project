d5e182368c83c8c02629f028f0fd041aa429f4d0
admin <admin@macbook-pro-13.home>
2019-03-23 15:38:01 +0000

    Implementation GetProductCodeById

--- src/main/java/DAO/ProductCodeDAO.java
+++ src/main/java/DAO/ProductCodeDAO.java
@@ -5,10 +5,60 @@
  */
 package DAO;
 
+import database.DAOException;
+import java.sql.Connection;
+import models.Product;
+import models.Product_Code;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
+
+
 /**
  *
  * @author admin
  */
 public class ProductCodeDAO {
+
+    protected  DataSource  myDataSource ;
+    /*    private final DataSource myDataSource;*/
+    /*GetProductCodeByID(), GetAllProductCodes(),*/
     
+    public ProductCodeDAO(DataSource myDataSource) {
+        this.myDataSource = myDataSource;
+    }
+    
+    
+    public Product_Code GetProductCodeByID(int Prod_Code) throws DAOException {
+            
+        Product_Code pCode = new Product_Code();
+		String sql = "SELECT * FROM PRODUCT WHERE Prod_Code = ? ";
+		
+	try (Connection connection = myDataSource.getConnection(); // On crée un statement pour exécuter une requête
+			PreparedStatement stmt = connection.prepareStatement(sql)) {
+                     
+			stmt.setInt(1, Prod_Code);
+			try (ResultSet rs = stmt.executeQuery()) {
+				if (rs.next()) { // On a trouvé
+                                pCode.setDiscount_code(rs.getString("DISCOUNT_CODE"));
+                                pCode.setDescription(rs.getString("DESCRIPTION"));
+				} 
+                                
+			}
+		}  catch (SQLException ex) {
+			Logger.getLogger("DAO").log(Level.SEVERE, null, ex);
+			throw new DAOException(ex.getMessage());
+		}
+
+		return pCode;
+	} 
+
+
+
 }
