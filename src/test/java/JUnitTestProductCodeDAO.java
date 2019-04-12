7101b28c833caa2fa2c6547c6ddd14c9ca110961
admin <admin@macbook-pro-13.home>
2019-04-12 12:40:16 +0100

    Test Product Code DAO

---
+++ src/test/java/JUnitTestProductCodeDAO.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author admin
+ */
+public class JUnitTestProductCodeDAO {
+    
+    
+    
+    public JUnitTestProductCodeDAO() {
+       /* 
+        private ProductCodeDAO PCDAO;
+        String Prod_Code;
+        String discount_code;
+        String description;
+        */
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+      /*PCDAO= new ProductCodeDAO(Database.getDataSource());
+        Prod_Code = ;
+        discount_code = ;
+        description = ;
+       */ 
+    }
+    
+    @After
+    public void tearDown() {
+    }
+/* 
+    @Test
+    public void TestGetProductCodeByID() throws DAOException { 
+         Product_Code user = PCDAO.GetProductCodeByID(Prod_Code);
+         assertEquals("// ???", user.getName()); ????????
+    @Test
+    public void TestGetAllProductCodes() throws DAOException { 
+         Product_Code user = PCDAO.GetGetAllProductCodes();
+         assertEquals("// ???", user.getName()); ????????
+    
+    
+    */
+}
